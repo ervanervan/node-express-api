48f3e8d52afdb561c857e1a9afc45112037d346f
ervanervan <ervanervan040@gmail.com>
2024-01-04 17:12:45 +0700

    Finish GET /users finds all users

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,23 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/users', (req, res) => {
-    res.send('Helo from users')
+const users = [
+    {
+        firstName: "Jhon",
+        lastName: "Doe",
+        age: 25
+    },
+    {
+        firstName: "Jane",
+        lastName: "Doe",
+        age: 25
+    }
+]
+
+// all routes in here are starting with /users
+router.get('/', (req, res) => {
+    console.log(users)
+    res.send(users)
 })
 
 export default router;
